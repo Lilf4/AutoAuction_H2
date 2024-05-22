caf58f4044f90f0ae035c2818fb1e20e36a92a7e
Lilly <47436048+Lilf4@users.noreply.github.com>
2024-05-22 10:53:10 +0200

    Moved docs folder, added basic logic to login/create user butto

--- AutoAuction/ViewModels/LoginViewModel.cs
+++ AutoAuction/ViewModels/LoginViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +15,10 @@ namespace AutoAuction.ViewModels {
         public void Login() {
             MainWindowViewModel.Instance.CurrViewModel = new HomeViewModel();
         }
+
+        public void CreateUser() {
+            MainWindowViewModel.Instance.CurrViewModel = new UserCreationViewModel();
+        }
     }
+
 }
