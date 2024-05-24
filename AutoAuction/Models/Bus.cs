54710642699a08d3b27b4b3a740ab2662e63189f
lilf4 <danielfandersen@hotmail.com>
2024-05-24 08:35:43 +0200

    Created Bus and Truck classes with necessary fields V7,V8,V9,V10,V11,V12

---
+++ AutoAuction/Models/Bus.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoAuction.Models {
+    public class Bus : HeavyVehicle {
+
+        private double motorSize;
+        public override double MotorSize { 
+            get { return motorSize; }
+            set {
+                if (value < 4.2 || value > 15.0)
+                {
+                    throw new ArgumentOutOfRangeException("Motor size must be between 4.2 and 15.0");
+                }
+                motorSize = value;
+            } 
+        }
+
+        public int Seats { get; set; }
+        public int StandingPlaces { get; set; }
+        public bool Toilets { get; set; }
+
+
+        //TODO sort by necessary start set fields
+        public Bus(int id, string name, int kmDriven, string regCode, int year, bool towHook, double kmPerUnit, FuelTypes fueltType, float height, float length, float weight, int seats, int standingPlaces, bool toilets) : 
+            base(id, name, kmDriven, regCode, year, towHook, kmPerUnit, fueltType, height, length, weight) {
+            this.Seats = seats;
+            this.StandingPlaces = standingPlaces;
+            this.Toilets = toilets;
+
+            if(towHook) {
+                LicenseType = LicenseTypes.DE;
+            }
+            else {
+                LicenseType = LicenseTypes.D;
+            }
+        }
+
+        public override string ToString() {
+            return base.ToString() + $", Vehicle Type: Bus";
+        }
+    }
+}
---
+++ AutoAuction/Models/Truck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoAuction.Models {
+    internal class Truck : HeavyVehicle {
+        private double motorSize;
+        public override double MotorSize {
+            get {
+                return motorSize;
+            }
+            set {
+                if (value < 4.2 || value > 15.0) {
+                    throw new ArgumentOutOfRangeException("Motor size must be between 4.2 and 15.0");
+                }
+                motorSize = value;
+            }
+        }
+
+        public int LoadCapacity { get; set; }
+
+        public Truck(int id, string name, int kmDriven, string regCode, int year, bool towHook, double kmPerUnit, FuelTypes fueltType, float height, float length, float weight, int LoadCapacity)
+        : base(id, name, kmDriven, regCode, year, towHook, kmPerUnit, fueltType, height, length, weight) {
+            this.LoadCapacity = LoadCapacity;
+
+            if (towHook) {
+                LicenseType = LicenseTypes.CE;
+            }
+            else {
+                LicenseType = LicenseTypes.C;
+            }
+        }
+
+        public override string ToString() {
+            return base.ToString() + $", Vehicle Type: Truck";
+        }
+    }
+}
