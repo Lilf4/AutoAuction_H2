a215c95a4cd91862390a2784920c834e81a8d1d3
Philip Kjær <philip.kjaer@man-es.com>
2024-06-12 10:42:04 +0200

    V13, V14, V15 - Created PersonalCar

--- AutoAuction/Models/PersonalCar.cs
+++ AutoAuction/Models/PersonalCar.cs
@@ -6,23 +6,20 @@ using System.Threading.Tasks;
 
 namespace AutoAuction.Models
 {
-	public abstract class PersonalCar : Vehicle
-	{
+	public abstract class PersonalCar : Vehicle	{
 
 		//Shared properties for Personal cars
 		public int NumberOfSeats { get; set; }
 		public int BootSize { get; set; }
 
 		public PersonalCar(int id, string name, int kmDriven, string regCode, int year, bool towHook, double kmPerUnit, FuelTypes fueltType, int bootSize, int numberOfSeats)
-		: base(id, name, kmDriven, regCode, year, towHook, kmPerUnit, fueltType)
-		{
+		: base(id, name, kmDriven, regCode, year, towHook, kmPerUnit, fueltType) { 
 			this.NumberOfSeats = numberOfSeats;
 			this.BootSize = bootSize;
 		}
 
 
-		public override string ToString()
-		{
+		public override string ToString() {
 			return base.ToString() + $", Vehicle Class: PersonalCar";
 		}
 	}
