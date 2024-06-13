793343ddaa2ab3e29ad91f2e97f930b21ead4a92
Dagor94 <Philler9460@gmail.com>
2024-06-13 21:42:36 +0200

    Updated SQL Script
    Added Triggers, Tables and Stored Procedures.
    
    -- Triggers
      - trg_ProfileCreation - Done
      - trg_ProfileDeletion - Done
      - trg_AuctionCreation - Done
      - trg_AuctionEnding - Done
    
     -- Tables
      - Logging Tables:
       - ProfileCreationLog - Done
       - ProfileDeletionLog - Done
       - AuctionCreationLog - Done
       -- AuctionEndingLog  - Done
    
     -- Stored Procedures
      - CreateAuction_sp - Done
      - PlaceBid_sp  - Done
      - EndAuction_sp - Done

--- Docs/Database/SQL_Script.sql
+++ Docs/Database/SQL_Script.sql
@@ -19,11 +19,15 @@
 			-- 12. ActiveAuctions
 			-- 13. FinishedAuctions
 			-- 14. Bids
-		-- Stored Procedures (4)
+		-- Stored Procedures (8)
 			-- 1. InsertPrivatePersonalCar_sp
 			-- 2. InsertProfessionalCar_sp
 			-- 3. InsertTruck_sp
 			-- 4. InsertBus_sp
+			-- 5. CreateNewUser_sp
+			-- 6. CreateAuction_sp
+			-- 7. PlaceBid_sp
+			-- 8. EndAuction_sp
 		-- Views (6)
 			-- 1. vw_CorporateUserDetails
 			-- 2. vw_PrivateUserDetails
@@ -35,19 +39,19 @@
 			-- UserCreatorRole
 		-- Logins
 			-- NewUserCreator
-		
+		-- Indexing (2)
+			-- 1. idx_username  - Users (Username)
+			-- 2. idx_vehicleid - Vehicles (Id)
+		-- Triggers (4)
+			-- 1. trg_ProfileCreation
+			-- 2. trg_ProfileDeletion
+			-- 3. trg_AuctionCreation
+			-- 4. trg_AuctionEnding
 
 -- TODO:
 	-- More Views?
 	-- More Stored Procedures?
 	-- Triggers
-	-- Auctions
-	-- Bids History
-	-- Log Profile Creation
-	-- Log Profile Deletion
-	-- Log Auction Creation
-	-- Log Auction Ending
-	-- Log Succesful Logins
 	-- Roles
 -----------------------------------
 -- DROP TABLES + DATABASE IF EXISTS
@@ -82,14 +86,19 @@ GO
 		-- Trucks
 		-- PrivatePersonalCars
 		-- ProfessionalCars
-	-- Auction tables
+	-- Auction tables:
 		-- Auctions 
 		-- ActiveAuctions
 		-- FinishedAuctions
 		-- Bids
+	-- Logging tables:
+		-- ProfileCreationLog
+		-- ProfileDeletionLog
+		-- AuctionCreationLog
+		-- AuctionEndingLog
 
 --------------
--- USER TABLES
+-- USER TABLES 1, 2, 3
 --------------
 CREATE TABLE Users (
 	Id INT PRIMARY KEY IDENTITY(1,1),
@@ -114,7 +123,7 @@ CREATE TABLE PrivateUsers (
 );
 
 -----------------
--- VEHICLE TABLES
+-- VEHICLE TABLES 4, 5, 6, 7, 8, 9, 10
 -----------------
 CREATE TABLE Vehicles (
 	Id INT PRIMARY KEY IDENTITY(1,1),
@@ -179,52 +188,83 @@ CREATE TABLE ProfessionalCars (
 	);
 
 -----------------
--- AUCTION TABLES
+-- AUCTION TABLES 11, 12, 13, 14
 -----------------
-	CREATE TABLE Auctions (
-	Id INT PRIMARY KEY IDENTITY(1,1),
-	SellerID INT,
-	FOREIGN KEY (SellerID) REFERENCES Users(Id),
-	VehicleID INT,
-	FOREIGN KEY (VehicleID) REFERENCES Vehicles(Id),
-	MinimumPrice DECIMAL NOT NULL
-	);
+CREATE TABLE Auctions (
+Id INT PRIMARY KEY IDENTITY(1,1),
+SellerID INT,
+FOREIGN KEY (SellerID) REFERENCES Users(Id),
+VehicleID INT,
+FOREIGN KEY (VehicleID) REFERENCES Vehicles(Id),
+MinimumPrice DECIMAL NOT NULL
+);
 
-	CREATE TABLE ActiveAuctions (
-	id INT PRIMARY KEY IDENTITY (1,1),
-	AuctionID INT,
-	FOREIGN KEY (AuctionID) REFERENCES Auctions(Id)
-	);
+CREATE TABLE ActiveAuctions (
+id INT PRIMARY KEY IDENTITY (1,1),
+AuctionID INT,
+FOREIGN KEY (AuctionID) REFERENCES Auctions(Id)
+);
 
-	CREATE TABLE FinishedAuctions (
-	id INT PRIMARY KEY IDENTITY (1,1),
-	AuctionID INT,
-	FOREIGN KEY (AuctionID) REFERENCES Auctions(Id)
-	);
+CREATE TABLE FinishedAuctions (
+id INT PRIMARY KEY IDENTITY (1,1),
+AuctionID INT,
+FOREIGN KEY (AuctionID) REFERENCES Auctions(Id)
+);
 
-	CREATE TABLE Bids (
-	id INT PRIMARY KEY IDENTITY (1,1),
-	AuctionID INT,
-	FOREIGN KEY (AuctionID) REFERENCES Auctions(Id),
-	BidderID INT,
-	FOREIGN KEY (BidderID) REFERENCES Users(Id),
-	BidAmount DECIMAL NOT NULL
-	);
-GO;
+CREATE TABLE Bids (
+id INT PRIMARY KEY IDENTITY (1,1),
+AuctionID INT,
+FOREIGN KEY (AuctionID) REFERENCES Auctions(Id),
+BidderID INT,
+FOREIGN KEY (BidderID) REFERENCES Users(Id),
+BidAmount DECIMAL NOT NULL
+);
+GO
 
+-----------------
+-- LOGGING TABLES
+-----------------
+CREATE TABLE ProfileCreationLog (
+LogId INT PRIMARY KEY IDENTITY(1,1),
+Username VARCHAR(50),
+CreatedAt DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE ProfileDeletionLog (
+LogId INT PRIMARY KEY IDENTITY(1,1),
+Username VARCHAR(50),
+DeletedAt DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE AuctionCreationLog (
+LogId INT PRIMARY KEY IDENTITY(1,1),
+AuctionID INT,
+SellerID INT,
+CreatedAt DATETIME DEFAULT GETDATE()
+);
+
+ CREATE TABLE AuctionEndingLog (
+LogId INT PRIMARY KEY IDENTITY(1,1),
+AuctionID INT,
+EndedAt DATETIME DEFAULT GETDATE()
+);
+GO
 
 -------------------------------------
 -- STORED PROCEDURES - suffixed '_sp'
 -------------------------------------
 -- Contains:
-	-- InsertPrivatePersonalCar_sp
-	-- InsertProfessionalCar_sp
-	-- InsertTruck_sp
-	-- InsertBus_sp
-	-- CreateNewUser_sp
+	-- 1. InsertPrivatePersonalCar_sp
+	-- 2. InsertProfessionalCar_sp
+	-- 3. InsertTruck_sp
+	-- 4. InsertBus_sp
+	-- 5. CreateNewUser_sp
+	-- 6. CreateAuction_sp
+	-- 7. PlaceBid_sp
+	-- 8. EndAuction_sp - WIP
 
 --------------------------------------------
--- InsertPrivatePersonalCar Stored Procedure
+-- 1. InsertPrivatePersonalCar Stored Procedure
 --------------------------------------------
 CREATE PROCEDURE InsertPrivatePersonalCar_sp
 	@Name NVARCHAR(50),
@@ -272,10 +312,10 @@ BEGIN
 		THROW;
 	END CATCH
 END;
-GO;
+GO
 
 ------------------------------------------
--- Insert ProfessionalCar Stored Procedure
+-- 2. Insert ProfessionalCar Stored Procedure
 ------------------------------------------
 CREATE PROCEDURE InsertProfessionalCar_sp
 	@Name NVARCHAR(50),
@@ -324,10 +364,10 @@ BEGIN
 		THROW;
 	END CATCH
 END;
-GO;
+GO
 
 --------------------------------
--- Insert Truck Stored Procedure
+-- 3. Insert Truck Stored Procedure
 --------------------------------
 CREATE PROCEDURE InsertTruck_sp
     @Name NVARCHAR(100),
@@ -374,10 +414,10 @@ BEGIN
         THROW;
     END CATCH
 END;
-GO;
+GO
 
 ------------------------------
--- Insert Bus Stored Procedure
+-- 4. Insert Bus Stored Procedure
 ------------------------------
 CREATE PROCEDURE InsertBus_sp
     @Name NVARCHAR(100),
@@ -426,10 +466,10 @@ BEGIN
         THROW;
     END CATCH
 END;
-GO;
+GO
 
 -----------------------------------
--- Create New User Stored Procedure
+-- 5. Create New User Stored Procedure
 -----------------------------------
 CREATE PROCEDURE CreateNewUser_sp
 	@Username VARCHAR(50),
@@ -477,7 +517,105 @@ BEGIN
 		THROW;
 	END CATCH
 END;
-GO;
+GO
+
+----------------------------------
+-- 6. Create Auction Stored Procedure
+----------------------------------
+CREATE PROCEDURE CreateAuction_sp
+	@SellerID INT,
+	@VehicleID INT,
+	@MinimumPrice DECIMAL(19, 4)
+AS
+BEGIN
+	BEGIN TRANSACTION;
+
+	BEGIN TRY
+		INSERT INTO Auctions (SellerID, VehicleID, MinimumPrice)
+		VALUES (@SellerID, @VehicleID, @MinimumPrice);
+
+		DECLARE @AuctionID INT;
+		SET @AuctionID = SCOPE_IDENTITY();
+
+		INSERT INTO ActiveAuctions (AuctionID)
+		VALUES (@AuctionID);
+
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION;
+		THROW 50000, 'ERROR - Auction Was Not Created', 1;
+	END CATCH
+END;
+GO
+
+-----------------------------
+-- 7. Place Bid Stored Procedure
+-----------------------------
+CREATE PROCEDURE PlaceBid_sp
+	@AuctionID INT,
+	@BidderID INT,
+	@BidAmount Decimal(19, 4)
+AS
+BEGIN
+	BEGIN TRANSACTION;
+
+    BEGIN TRY
+		IF NOT EXISTS (SELECT 1 FROM ActiveAuctions WHERE AuctionID = @AuctionID)
+        BEGIN
+			THROW 50001, 'ERROR - Auction is not active or does not exist.', 1;
+		END
+
+		IF EXISTS (SELECT 1 FROM Bids WHERE AuctionID = @AuctionID)
+		BEGIN
+			DECLARE @CurrentHighestBid DECIMAL (19, 4);
+			SELECT @CurrentHighestBid = MAX(BidAmount) FROM Bids WHERE AuctionID = @AuctionID;
+
+			IF @BidAmount <= @CurrentHighestBid
+			BEGIN
+				THROW 50002, 'ERROR - Bid must be higher than the current bid.', 2;
+			END
+		END
+
+		INSERT INTO Bids (AuctionID, BidderID, BidAmount)
+		VALUES (@AuctionID, @BidderID, @BidAmount);
+
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION;
+		THROW 50000, 'ERROR - Could not place Bid.', 3;
+	END CATCH
+END;
+GO
+
+-------------------------------
+-- 8. End Auction Stored Procedure
+-------------------------------
+CREATE PROCEDURE EndAuction_sp
+	@AuctionID INT
+AS
+BEGIN
+	BEGIN TRANSACTION;
+
+	BEGIN TRY
+		IF NOT EXISTS (SELECT 1 FROM ActiveAuctions WHERE AuctionID = @AuctionId)
+		BEGIN
+			THROW 50001, 'ERROR - Auction is not active or does not exists.', 1;
+		END
+
+		INSERT INTO FinishedAuctions (AuctionID)
+		SELECT  AuctionID From ActiveAuctions Where AuctionID = @AuctionID;
+		DELETE FROM ActiveAuctions Where AuctionID = @AuctionID;
+
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION;
+		THROW 50000, 'ERROR - Could not end Auction', 2;
+	END CATCH
+END;
+GO
 
 
 -------------------------
@@ -506,7 +644,7 @@ FROM
     Users u
 JOIN
     CorporateUsers cu ON u.Username = cu.Username;
-GO;
+GO
 
 ------------------------
 -- vw_PrivateUserDetails
@@ -522,7 +660,7 @@ FROM
     Users u
 JOIN
     PrivateUsers pu ON u.Username = pu.Username;
-GO;
+GO
 
 -----------------------
 -- vw_PrivateCarDetails
@@ -549,7 +687,7 @@ CREATE VIEW vw_PrivateCarDetails AS
 		PersonalCars pc ON v.Id = pc.VehicleID
 	JOIN 
 		PrivatePersonalCars ppc ON pc.Id = ppc.PersonalCarID;
-GO;
+GO
 
 ----------------------------
 -- vw_ProfessionalCarDetails
@@ -577,7 +715,7 @@ CREATE VIEW vw_ProfessionalCarDetails AS
 		PersonalCars pc ON v.Id = pc.VehicleID
 	JOIN 
 		ProfessionalCars pfc ON pc.Id = pfc.PersonalCarID;
-GO;
+GO
 
 ------------------
 -- vw_TruckDetails
@@ -605,7 +743,7 @@ CREATE VIEW vw_TruckDetails AS
 		HeavyVehicles hv ON v.Id = hv.VehicleID
 	JOIN 
 		Truck t ON hv.Id = t.HeavyVehicleID;
-GO;
+GO
 
 ----------------
 -- vw_BusDetails
@@ -635,7 +773,7 @@ CREATE VIEW vw_BusDetails AS
 		HeavyVehicles hv ON v.Id = hv.VehicleID
 	JOIN 
 		Bus b ON hv.Id = b.HeavyVehicleID;
-GO;
+GO
 
 
 -------------------------
@@ -643,7 +781,7 @@ GO;
 -------------------------
 CREATE ROLE UserCreatorRole;
 GRANT EXECUTE ON OBJECT::dbo.CreateNewUser_sp TO UserCreatorRole;
-
+GO
 
 -----------------------
 -- LOGINS - CreatorUser
@@ -653,3 +791,93 @@ CREATE LOGIN NewUserCreator WITH PASSWORD = 'Creator123!';
 	USE AutoAuctionDB;
 	CREATE USER NewUserCreator FOR LOGIN NewUserCreator;
 	ALTER ROLE UserCreatorRole ADD MEMBER NewUserCreator;
+GO
+
+
+---------------------------
+-- INDEXS - prefixed 'inx_'
+---------------------------
+-- Contains:
+	-- idx_username -  Users (Username)
+	-- idx_vehicleid - Vehicles (Id)
+
+-----------------------------------
+-- idx_username -  Users (Username)
+-----------------------------------
+CREATE INDEX idx_username ON Users (Username);
+
+--------------------------------
+-- idx_vehicleid - Vehicles (Id)
+--------------------------------
+CREATE INDEX idx_vehicleid ON Vehicles (Id);
+GO
+
+----------------------------
+-- TRIGGERS - prefixed 'trg'
+----------------------------
+-- Contains:
+	-- trg_ProfileCreation	- WIP
+	-- trg_ProfileDeletion	- WIP
+	-- trg_AuctionCreation	- WIP
+	-- trg_AuctionEnding	- WIP
+
+---------------------------
+-- Profile Creation Trigger
+---------------------------
+CREATE TRIGGER trg_ProfileCreation
+ON Users
+AFTER INSERT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO ProfileCreationLog (Username)
+	SELECT Username FROM inserted;
+END;
+GO
+
+---------------------------
+-- Profile Deletion Trigger
+---------------------------
+CREATE TRIGGER trg_ProfileDeletion
+ON Users
+AFTER DELETE
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO ProfileDeletionLog (Username)
+	SELECT Username FROM deleted;
+END;
+GO
+
+---------------------------
+-- Auction Creation Trigger
+---------------------------
+CREATE TRIGGER trg_AuctionCreation
+ON Auctions
+AFTER INSERT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO AuctionCreationLog (AuctionID, SellerID)
+	SELECT id, SellerID FROM inserted;
+END;
+GO
+
+-------------------------
+-- Auction Ending Trigger
+-------------------------
+CREATE TRIGGER trg_AuctionEnding
+ON FinishedAuctions
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO AuctionEndingLog (AuctionID)
+    SELECT AuctionID FROM inserted;
+END;
+GO
+
