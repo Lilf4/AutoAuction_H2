8938af3d96419cfb4fb2f161ad610bceaaaba548
lilf4 <danielfandersen@hotmail.com>
2024-06-14 10:33:23 +0200

    Created privateuser according to points U10-U12

---
+++ AutoAuction/Models/PrivateUser.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoAuction.Models {
+    public class PrivateUser : User {
+        public string CPR { get; set; }
+
+        public PrivateUser(int id, string username, int postcode, decimal balance, string CPR) : base(id, username, postcode, balance) {
+            this.CPR = CPR;
+        }
+
+        public override string ToString() {
+            return base.ToString() + $", CPR: {CPR}";
+        }
+    }
+}
