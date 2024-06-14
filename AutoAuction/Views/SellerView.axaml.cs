913d951a32291038990c05497befeace8792849b
lilf4 <danielfandersen@hotmail.com>
2024-06-14 10:36:03 +0200

    Created Seller screen, G6

---
+++ AutoAuction/Views/SellerView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AutoAuction.Views;
+
+public partial class SellerView : UserControl
+{
+    public SellerView()
+    {
+        InitializeComponent();
+    }
+}
