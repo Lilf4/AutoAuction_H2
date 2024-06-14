7a08b5e437fd2add9ff1ff2e548cef9443f3646b
lilf4 <danielfandersen@hotmail.com>
2024-06-14 10:32:58 +0200

    Created coporateuser according to points U7-U9

---
+++ AutoAuction/Models/CoporateUser.cs
@@ -0,0 +1,15 @@
+﻿namespace AutoAuction.Models {
+    public class CoporateUser : User {
+
+        public decimal CreditLimit { get; set; }
+        public string CVR { get; set; }
+        public CoporateUser(int id, string username, int postcode, decimal balance, decimal creditLimit, string cvr) : base(id, username, postcode, balance) {
+            this.CreditLimit = creditLimit;
+            this.CVR = cvr;
+        }
+
+        public override string ToString() {
+            return base.ToString() + $", Credit Limit: {CreditLimit}, CVR: {CVR}";
+        }
+    }
+}
