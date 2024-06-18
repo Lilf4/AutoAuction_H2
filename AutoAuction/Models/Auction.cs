8160f27a136c9e050942e9bcf27f0d16b72ab373
Philip Kjær <philip.kjaer@man-es.com>
2024-06-18 08:55:28 +0200

    A1, A2,

---
+++ AutoAuction/Models/Auction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoAuction.Models
+{
+    public class Auction
+    {
+        public int AuctionID { get; set; }
+        public User? Seller { get; set; }
+        public Vehicle? Vehicle { get; set; } 
+        public decimal MinimumPrice { get; set; }
+        public DateTime StartDate { get; set; }
+        
+        public Auction(int auctionID, User? seller, Vehicle? vehicle, decimal minimumPrice, DateTime startDate)
+        {
+            AuctionID = auctionID;
+            Seller = seller;
+            Vehicle = vehicle;
+            MinimumPrice = minimumPrice;
+            StartDate = startDate;
+        }
+    }
+
+
+    
+
+
+}
