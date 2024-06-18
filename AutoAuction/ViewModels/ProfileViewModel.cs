2577256a696d2151544a6ad67362fad24b199254
Philip Kjær <philip.kjaer@man-es.com>
2024-06-18 13:25:04 +0200

    Added Profile View

---
+++ AutoAuction/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,24 @@
+﻿using AutoAuction.Models;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AutoAuction.Models.Vehicle;
+
+namespace AutoAuction.ViewModels {
+    public class ProfileViewModel : ViewModelBase {
+
+        private User observableUser;
+        public User ObservableUser
+        {
+            get => observableUser;
+            set => this.RaiseAndSetIfChanged(ref observableUser, value);
+        }
+        public ProfileViewModel() {
+            ObservableUser = new(1, "Test@TestUser.com", 1234, 0);
+        }
+    }
+}
