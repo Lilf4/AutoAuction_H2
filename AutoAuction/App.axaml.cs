440cce860682484ce679661b36dd3c3a0e856af5
lilf4 <danielfandersen@hotmail.com>
2024-06-18 15:23:58 +0200

    Added repository injection on startup

--- AutoAuction/App.axaml.cs
+++ AutoAuction/App.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using AutoAuction.ViewModels;
 using AutoAuction.Views;
+using AutoAuction.DAL;
 
 namespace AutoAuction;
 
@@ -13,13 +14,17 @@ public partial class App : Application
         AvaloniaXamlLoader.Load(this);
     }
 
+    static IAuction auctionR = new AuctionR();
+    static IUser userR = new UserR();
+    static IVehicle vehicleR = new VehicleR();
+
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = new MainWindowViewModel(auctionR, userR, vehicleR),
             };
         }
 
