41fa01f5e058eaea5ce0049abeaff2c559b03ed4
Philip Kjær <philip.kjaer@man-es.com>
2024-06-18 09:44:09 +0200

    Updated Auction constructor and fields  to not allow it to be nullable.

--- AutoAuction/Models/Auction.cs
+++ AutoAuction/Models/Auction.cs
@@ -9,12 +9,12 @@ namespace AutoAuction.Models
     public class Auction
     {
         public int AuctionID { get; set; }
-        public User? Seller { get; set; }
-        public Vehicle? Vehicle { get; set; } 
+        public User Seller { get; set; }
+        public Vehicle Vehicle { get; set; } 
         public decimal MinimumPrice { get; set; }
         public DateTime StartDate { get; set; }
         
-        public Auction(int auctionID, User? seller, Vehicle? vehicle, decimal minimumPrice, DateTime startDate)
+        public Auction(int auctionID, User seller, Vehicle vehicle, decimal minimumPrice, DateTime startDate)
         {
             AuctionID = auctionID;
             Seller = seller;
